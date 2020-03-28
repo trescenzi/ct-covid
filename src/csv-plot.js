3c1f0e28637858f45377425cc00965a179c40f78
Thomas Crescenzi <trescenzi@gmail.com>
2020-03-27 23:00:17 -0400

    remove console logs

--- src/csv-plot.js
+++ src/csv-plot.js
@@ -1,8 +1,8 @@
-import { Plot } from './preact-plotly.js';
-import { LoadingSpinner } from './loading.js';
-import { fetchCSV } from './csv.js';
-import { useEffect, useState } from 'preact/hooks';
-import { html } from 'htm/preact';
+import { Plot } from "./preact-plotly.js";
+import { LoadingSpinner } from "./loading.js";
+import { fetchCSV } from "./csv.js";
+import { useEffect, useState } from "preact/hooks";
+import { html } from "htm/preact";
 
 const options = {
   displayModeBar: false,
@@ -10,36 +10,36 @@ const options = {
 };
 export function CSVPlot({
   csvName,
-  xColumn = 'Date',
+  xColumn = "Date",
   yColumns,
   chartOptions = {},
   layoutOptions = {},
 }) {
   const [chartData, setState] = useState();
   useEffect(() => {
-    fetchCSV(csvName).then(({data}) => setState(
-      yColumns.map(columnName => ({
-        x: data.map(row => row[xColumn]),
-        y: data.map(row => row[columnName]),
-        name: columnName,
-        type: 'line',
-      }))
-    ));
+    fetchCSV(csvName).then(({ data }) =>
+      setState(
+        yColumns.map((columnName) => ({
+          x: data.map((row) => row[xColumn]),
+          y: data.map((row) => row[columnName]),
+          name: columnName,
+          type: "line",
+        }))
+      )
+    );
   }, []);
-  console.log(chartData);
   return html`
-    <div class='chart-container'>
+    <div class="chart-container">
       ${!chartData
         ? html`<${LoadingSpinner} />`
         : html`<${Plot}
-          data=${chartData}
-          layout=${layoutOptions}
-          options=${{
-            ...options,
-            ...chartOptions,
-          }}
-        />`
-      }
+            data=${chartData}
+            layout=${layoutOptions}
+            options=${{
+              ...options,
+              ...chartOptions,
+            }}
+          />`}
     </div>
   `;
 }
--- src/csv.js
+++ src/csv.js
@@ -1,23 +1,31 @@
 export function fetchCSV(name) {
   return fetch(`/data/${name}.csv`)
-    .then(response => response.text())
-    .then(processCSV)
+    .then((response) => response.text())
+    .then(processCSV);
 }
 
-
 function processCSV(csv) {
-  const [rawHeaders, ...body] = csv.split('\n');
-  const headers = rawHeaders.split(',');
+  const [rawHeaders, ...body] = csv.split("\n");
+  const headers = rawHeaders.split(",");
 
-  console.log(headers, body);
   return {
     headers,
-    data: body.map(row => row.split(',')).reduce((rowObjects, row) => ([
-      ...rowObjects,
-      {...row.reduce((rowObject, val, i) => ({
-        ...rowObject,
-        [headers[i]]: val,
-      }), {})}
-    ]), []),
+    data: body
+      .map((row) => row.split(","))
+      .reduce(
+        (rowObjects, row) => [
+          ...rowObjects,
+          {
+            ...row.reduce(
+              (rowObject, val, i) => ({
+                ...rowObject,
+                [headers[i]]: val,
+              }),
+              {}
+            ),
+          },
+        ],
+        []
+      ),
   };
 }
