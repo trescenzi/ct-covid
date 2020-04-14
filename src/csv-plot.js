e86d0990086c73525f4f6d106d468444934c2dfe
Thomas Crescenzi <trescenzi@gmail.com>
2020-04-14 19:33:47 -0400

    feat(data): fetch from state api

--- src/csv-plot.js
+++ src/csv-plot.js
@@ -20,7 +20,7 @@ const colors2 = [
 ];
 export function CSVPlot({
   csv,
-  xColumn = "Date",
+  xColumn = "date",
   yColumns,
   chartOptions = {},
   layoutOptions = {},
@@ -91,47 +91,45 @@ export function RateOfGrowthCSVChart({
       <div class="chart-container-title">
         ${layoutOptions.title}
       </div>
-      <div class="chart-container-header"/>
-      ${
-        !csv
-          ? html`<${LoadingSpinner} />`
-          : html`<${Plot}
-              data=${chartData}
-              layout=${{
-                ...layoutOptions,
-                title: '',
-                legend: {
-                  x: 0,
-                  xanchor: 'top',
-                  y: 1
-                },
-                margin: {
-                  l: 40,
-                  r: 10,
-                  b: 30,
-                  t: 0,
-                  pad: 0
-                },
-                height: 390,
-                yaxis: {
-                  ...layoutOptions.yaxis,
-                  type: "log",
-                  autorange: true,
-                  title: "Increase from Previous Day",
-                },
-                xaxis: {
-                  ...layoutOptions.xaxis,
-                  type: "log",
-                  autorange: true,
-                  title: "Total Number of Cases",
-                },
-              }}
-              options=${{
-                ...options,
-                ...chartOptions,
-              }}
-            />`
-      }
+      <div class="chart-container-header" />
+      ${!csv
+        ? html`<${LoadingSpinner} />`
+        : html`<${Plot}
+            data=${chartData}
+            layout=${{
+              ...layoutOptions,
+              title: "",
+              legend: {
+                x: 0,
+                xanchor: "top",
+                y: 1,
+              },
+              margin: {
+                l: 40,
+                r: 10,
+                b: 30,
+                t: 0,
+                pad: 0,
+              },
+              height: 390,
+              yaxis: {
+                ...layoutOptions.yaxis,
+                type: "log",
+                autorange: true,
+                title: "Increase from Previous Day",
+              },
+              xaxis: {
+                ...layoutOptions.xaxis,
+                type: "log",
+                autorange: true,
+                title: "Total Number of Cases",
+              },
+            }}
+            options=${{
+              ...options,
+              ...chartOptions,
+            }}
+          />`}
     </div>
   `;
 }
--- src/csv.js
+++ src/csv.js
@@ -1,7 +1,164 @@
+import takeWhile from "lodash-es/takeWhile.js";
+import groupBy from "lodash-es/groupBy.js";
+const DATA_STATEWIDE = `https://data.ct.gov/resource/rf3k-f8fg.json?$$app_token=${process.env.CT_GOV_APP_TOKEN}`;
+const DATA_BY_COUNTY = `https://data.ct.gov/resource/bfnu-rgqt.json?$$app_token=${process.env.CT_GOV_APP_TOKEN}`;
+const DATA_BY_TOWN = `https://data.ct.gov/resource/28fr-iqnx.json?$$app_token=${process.env.CT_GOV_APP_TOKEN}&$limit=50000`;
+const DATE_UPDATED = `https://data.ct.gov/resource/rf3k-f8fg.json?$$app_token=${process.env.CT_GOV_APP_TOKEN}&$select=date`;
+
+const json = (r) => r.json();
+export function getDateUpdated() {
+  return fetch(DATE_UPDATED)
+    .then((x) => x.json())
+    .then((dates) => new Date(dates[dates.length - 1].date));
+}
+export function fetchDataSet(kind) {
+  let governmentData;
+  let transcribedData;
+  switch (kind) {
+    case "STATE":
+      if (cachedCSVs.STATE) {
+        return Promise.resolve(cachedCSVs.STATE);
+      }
+      // the state data doesn't go back to the first cases so we have to merge
+      // it with the hand transcribed data
+      governmentData = fetch(DATA_STATEWIDE).then(json);
+      transcribedData = Promise.all([
+        fetchCSV("cases"),
+        fetchCSV("deaths"),
+        fetchCSV("hospitalizations"),
+      ]).then(
+        ([{ data: cases }, { data: deaths }, { data: hospitalizations }]) =>
+          cases.map((day, i) => ({
+            date: day.Date,
+            cases: day.Overall,
+            deaths: deaths[i].Overall,
+            hospitalizations: hospitalizations[i].Overall,
+            state: "CONNECTICUT",
+          }))
+      );
+      return Promise.all([governmentData, transcribedData]).then(mergeData);
+    case "COUNTY":
+      governmentData = fetch(DATA_BY_COUNTY)
+        .then(json)
+        .then((data) => processDataByLocation(data, "county"));
+      transcribedData = Promise.all([
+        fetchCSV("cases"),
+        fetchCSV("deaths"),
+        fetchCSV("hospitalizations"),
+      ]).then(
+        ([{ data: cases }, { data: deaths }, { data: hospitalizations }]) => ({
+          cases: cases.map((data) => ({
+            date: data.Date,
+            ...data,
+          })),
+          deaths: deaths.map((data) => ({
+            date: data.Date,
+            ...data,
+          })),
+          hospitalizations: hospitalizations.map((data) => ({
+            date: data.Date,
+            ...data,
+          })),
+        })
+      );
+      return Promise.all([governmentData, transcribedData]).then(
+        ([
+          {
+            cases: govCases,
+            deaths: govDeaths,
+            hospitalizations: govHospitalizations,
+          },
+          {
+            cases: transcribedCases,
+            deaths: transcribedDeaths,
+            hospitalizations: transcribedHospitalizations,
+          },
+        ]) => ({
+          cases: mergeData([govCases, transcribedCases]),
+          deaths: mergeData([govDeaths, transcribedDeaths]),
+          hospitalizations: mergeData([
+            govHospitalizations,
+            transcribedHospitalizations,
+          ]),
+        })
+      );
+    case "TOWN":
+      return fetch(DATA_BY_TOWN)
+        .then(json)
+        .then((data) => processDataByLocation(data, "town"));
+  }
+}
+
+function processDataByLocation(data, labelKey) {
+  return Object.values(
+    groupBy(data, ({ lastupdatedate }) => lastupdatedate)
+  ).reduce(
+    (aggregateData, day) => {
+      const dayData = day.reduce(
+        (breakdown, value) => ({
+          cases: {
+            ...breakdown.cases,
+            date: value.lastupdatedate,
+            [value[labelKey]]: value.confirmedcases,
+          },
+          deaths: {
+            ...breakdown.deaths,
+            date: value.lastupdatedate,
+            [value[labelKey]]: value.deaths,
+          },
+          hospitalizations: {
+            ...breakdown.hospitalizations,
+            date: value.lastupdatedate,
+            [value[labelKey]]: value.hospitalizedcases,
+          },
+        }),
+        {}
+      );
+      return {
+        cases: [...aggregateData.cases, dayData.cases],
+        deaths: [...aggregateData.deaths, dayData.deaths],
+        hospitalizations: [
+          ...aggregateData.hospitalizations,
+          dayData.hospitalizations,
+        ],
+      };
+    },
+    { cases: [], deaths: [], hospitalizations: [] }
+  );
+}
+
+function mergeData([governmentData, transcribedData]) {
+  const firstGovernmentDate = new Date(governmentData[0].date);
+  return [
+    ...takeWhile(
+      transcribedData,
+      (day) => new Date(`${day.date}T00:00:00.000`) < firstGovernmentDate
+    ),
+    ...governmentData,
+  ];
+}
+window.fetchData = fetchDataSet;
+const cachedCSVs = {
+  cases: null,
+  deaths: null,
+  hospitalizations: null,
+  STATE: null,
+  COUNTY: null,
+  TOWN: null,
+};
 export function fetchCSV(name) {
-  return fetch(`/data/${name}.csv`)
+  if (cachedCSVs[name]) {
+    return Promise.resolve(cachedCSVs[name]);
+  }
+
+  cachedCSVs[name] = fetch(`/data/${name}.csv`)
     .then((response) => response.text())
-    .then(processCSV);
+    .then(processCSV)
+    .then((data) => {
+      cachedCSVs[name] = data;
+      return data;
+    });
+  return cachedCSVs[name];
 }
 
 function processCSV(csv) {
